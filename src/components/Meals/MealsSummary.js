039516cf103978ca6f53e015947ed5bdc984bc6d
prince saspara <princesaspara46@gmail.com>
2023-07-24 19:57:10 +0530

    add form

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,19 +1,17 @@
 import React from "react";
-import classes from './MealsSummary.module.css'
+import classes from "./MealsSummary.module.css";
 const MealsSummary = () => {
   return (
     <section className={classes.summary}>
       <h2>Delicios Food, Delivers To You</h2>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia, hic
-        dignissimos, pariatur beatae ipsam, totam architecto itaque vero eius
-        quae dicta magni doloremque!
+        dignissimos, pariatur beatae ipsam, totam architecto
       </p>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta
         obcaecati nostrum ullam natus inventore ratione neque, porro velit? Eos
-        vel nihil a, harum animi corrupti ipsum, tempore nesciunt fugit labore,
-        quas placeat exercitationem neque.
+        vel nihil a, harum animi corrupti ipsum,
       </p>
     </section>
   );
