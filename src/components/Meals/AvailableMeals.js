f4523df41389ffd1cef035d76e045e7f53c64ede
prince saspara <princesaspara46@gmail.com>
2023-07-24 16:25:26 +0530

    add meal item

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,40 +1,50 @@
-import React from 'react'
-import classes from './AvailabelMeals.module.css'
+import React from "react";
+import classes from "./AvailabelMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealsItem/MealItem";
 const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
-
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
 
 const AvailableMeals = () => {
-    const mealList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>)
+  const mealList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
   return (
     <section className={classes.meals}>
+      <Card>
         <ul>{mealList}</ul>
+      </Card>
     </section>
-  )
-}
+  );
+};
 
-export default AvailableMeals
+export default AvailableMeals;
