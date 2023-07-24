1453c87048c94383f3c018e7d81fe841132359b3
prince saspara <princesaspara46@gmail.com>
2023-07-24 20:53:33 +0530

    add modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import classes from "./Cart.module.css"
+import Modal from '../UI/Modal'
 const Cart = () => {
     const cartItems = <ul className={classes['cart-items']}>{[ { id:'c1', name: 'sushi', amount: 2 ,price: 299},].map((item)=> <li>{item.name}</li>)}</ul> 
   return (
-    <div>
+    <Modal>
         {cartItems}
         <div className={classes.total}>
             <span>Total Amount</span>
@@ -13,7 +14,7 @@ const Cart = () => {
             <button className={classes['button--alt']}>Close</button>
             <button className={classes.button}>Order</button>
         </div>
-    </div>
+    </Modal>
   )
 }
 
